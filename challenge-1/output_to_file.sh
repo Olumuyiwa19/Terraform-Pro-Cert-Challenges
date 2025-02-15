eaef5e7769cf907a7202e78e4ecac6ad57404220
olumuyiwa19 <kolayemi.olumuyiwa@gmail.com>
2025-02-15 13:23:18 -0500

    solved ch1|ts3 -- create output_to_file.sh script to pass the output values to text files

---
+++ challenge-1/output_to_file.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Output S3 bucket names to s3.txt
+terraform output -json s3_bucket > s3.txt
+
+# Output IAM user names to iam-users.txt
+terraform output -json user_name > iam-users.txt
+
+# Output Security Group ID to sg-combined.txt
+terraform output -json sg_name > sg-combined.txt
+
+# Output Security Group Rule ID to sg-combined.txt
+terraform output -json sg_rule_id >> sg-combined.txt
