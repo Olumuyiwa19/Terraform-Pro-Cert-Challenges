336f8432bd27f87c718177f7191565ba612a12bb
olumuyiwa19 <kolayemi.olumuyiwa@gmail.com>
2025-02-16 00:07:05 -0500

    fix typo in output_to_file.sh

--- challenge-1/output_to_file.sh
+++ challenge-1/output_to_file.sh
@@ -7,7 +7,7 @@ terraform output -json s3_bucket > s3.txt
 terraform output -json user_name > iam-users.txt
 
 # Output Security Group ID to sg-combined.txt
-terraform output -json sg_name > sg-combined.txt
+terraform output -json sg_id > sg-combined.txt
 
 # Output Security Group Rule ID to sg-combined.txt
 terraform output -json sg_rule_id >> sg-combined.txt
